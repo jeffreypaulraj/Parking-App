3c0931efd32ecdbd45b120ed62f8e9da12c16618
Akash Jain <10014285@sbstudents.org>
2019-11-02 17:32:51 -0400

    akash

---
+++ app/src/main/java/com/example/aj/AJ2.java
@@ -0,0 +1,14 @@
+package com.example.aj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AJ2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aj2);
+    }
+}
---
+++ app/src/main/java/com/example/aj/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.aj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this, AJ2.class);
+        startActivity(intent);
+    }
+}
